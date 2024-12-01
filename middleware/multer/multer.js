ab34f54641160357f2cbbc826ec767e94ba3c856
Liyanapk <liyana.kp8590@gmail.com>
2024-12-01 09:15:02 -0800

    feat: done pic  do email

--- middleware/multer/multer.js
+++ middleware/multer/multer.js
@@ -1,18 +1,26 @@
-import multer from 'multer'
+import multer from 'multer';
 
-
-
-
+// Define storage configuration
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, 'uploads/');  
-    },
-    filename: (req, file, cb) => {
-      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-      cb(null, uniqueSuffix + '-' + file.originalname); 
-    }
-  });
-  
-  const upload = multer({ storage: storage });
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/'); // Directory to store uploaded files
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    cb(null, uniqueSuffix + '-' + file.originalname); // Generate unique filename
+  },
+});
 
-  export { upload };
+// Multer configuration
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 2 * 1024 * 1024 }, // Limit file size to 2MB
+  fileFilter: (req, file, cb) => {
+    if (!file.mimetype.startsWith('image/')) {
+      return cb(new Error('Only image files are allowed!'), false);
+    }
+    cb(null, true);
+  },
+});
+
+export { upload };
